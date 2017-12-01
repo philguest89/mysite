8eb55f3e5d61c578073e5b8b95dde630eec9fda3
philguest89 <34139923+philguest89@users.noreply.github.com>
2017-12-01 21:46:45 +0000

    Initial commit

---
+++ AboutMe.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+	<title>Phils Website 2</title>
+	<link href="./stylesheet.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+  <ul class="navigation">
+    <li><img src="https://s3.amazonaws.com/codecademy-content/courses/web-101/unit-9/htmlcss1-img_logo-shiptoit.png" height="20px;"></li>
+    <li class="active">Action List</li>
+    <li>Profiles</li>
+    <li>Settings</li>
+  </ul>
+  <a href="./index.php">Home</a>
+  
+  <p class="test bold"> This is a paragraph too </p>
+  
+  </br></br>
+  
+  <p> This is another paragraph </p>
+  
+  <p class="test" id="article-title">And another</p>
+  
+  <div class="parent">
+	<p class="child">Another one too</p>
+	<p class="child2">Another one!!!!</p>
+  </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+	print 'test';
+?>
+
+<html>
+<head>
+	<title>Phils Website</title>
+	<link href="./stylesheet.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+  <ul class="navigation">
+    <li><img src="https://s3.amazonaws.com/codecademy-content/courses/web-101/unit-9/htmlcss1-img_logo-shiptoit.png" height="20px;"></li>
+    <li class="active">Action List</li>
+    <li>Profiles</li>
+    <li>Settings</li>
+  </ul>
+  
+	<h1> Hello My Name is Phil G</h1>
+	</br>
+	<a href="AboutMe.php">About Me</a>
+</body>
+</html>
